2d5b72b11b4bb03cc06d57d47029d5cccf9d7b1a
josepagan <josepaganrnj@outlook.com>
2026-04-24 19:54:18 -0400

    Declared my field variables in Class Transaction and loaded my transactions.csv file with transaction examples.

---
+++ src/main/java/com/pluralsight/Transaction.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class Transaction {
+    public String date;
+    public String time;
+    public String description;
+    public String vendor;
+    public double amount;
+}
