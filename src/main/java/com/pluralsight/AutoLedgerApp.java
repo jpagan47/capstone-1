a686f334c3f30774d6b7d0dd55b55aa908e183a1
josepagan <josepaganrnj@outlook.com>
2026-04-25 12:49:22 -0400

    Assigned the methods withing my Ledger sub-Menu, made more todos to stay on top of all the method logic that I need to fill out in the future, added comments to keep main goals in mind

--- src/main/java/com/pluralsight/AutoLedgerApp.java
+++ src/main/java/com/pluralsight/AutoLedgerApp.java
@@ -8,27 +8,29 @@ public class AutoLedgerApp {
     public static String filePath = "src/main/resources/transactions.csv";
     public static Scanner myScanner = new Scanner(System.in);
     public static String mainMenuPrompt = """
-                🏁............🏎💨..
-                ===== Main Menu =====
-                (D) Add Deposit
-                (P) Make Payment (Debit)
-                (L) Ledger
-                (X) Exit
-                """;
+            🏁............🏎💨..
+            ===== Main Menu =====
+            (D) Add Deposit
+            (P) Make Payment (Debit)
+            (L) Ledger
+            (X) Exit
+            """;
     public static String ledgerMenuPrompt = """
-                === Ledger Menu ===
-                (A) View All Deposits
-                (D) View Deposits
-                (P) View Payments
-                (R) Custom Report Search
-                (H) Back To Home Page
-                """;
+            === Ledger Menu ===
+            (A) View All
+            (D) View  Only Deposits
+            (P) View Only Payments
+            (R) Custom Report Search
+            (H) Back To Home Page
+            """;
+
     //All my code that needs to run
     public static void main(String[] args) {
         mainMenu();
         System.out.println("End of Application");
 
     }
+
     public static void mainMenu() {
         boolean running = true;
         do {
@@ -65,8 +67,42 @@ public class AutoLedgerApp {
     private static void ledgerMenu() {
         System.out.println(ledgerMenuPrompt);
         //todo Add a .ignorecase somehow into the usersInput
+        String userInput = myScanner.nextLine();
+        switch (userInput) {
+            case "A"://All - Display all entries
+                displayAllTransactions();
+                break;
+            case "D"://Deposits - Display only the entries that are deposits into the account
+                displayAllDeposits();
+                break;
+            case "P"://Payments - Display only the negative entries (or payments
+                displayAllPayments();
+                break;
+            case "R"://R) Reports - A new screen that allows the user to run pre-defined reports or to run a custom search
+                customReportSearch();
+                break;
+            case "H":
+//              backToHomePage();
+                break;
+        }
 
 
+    }
+
+    private static void displayAllTransactions() {
+        //todo - Display all entries
+    }
+
+    private static void displayAllDeposits() {
+       //todo - Display only the entries that are deposits into the account
+    }
+
+    private static void displayAllPayments() {
+        //todo  - Display only the negative entries (or payments)
+    }
+
+    private static void customReportSearch() {
+        //todo- A new screen that allows the user to run pre-defined reports or to run a custom search
     }
 
 
